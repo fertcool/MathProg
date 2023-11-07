cb54e34007a3763f2172e2890c4893352aafabdb
артём иващенко <fertcool@gmail.com>
2023-11-07 21:12:25 +0300

    добавлен метод ньютона и покоорд спуска

--- main.py
+++ main.py
@@ -20,15 +20,32 @@ def grad_f(z1: float, z2: float, z3: float):
     t = tape.gradient(f, [x1, x2, x3])
     return np.array([float(t[0]), float(t[1]), float(t[2])])
 
+# def hessian_f(z1: float, z2: float, z3: float):
+#     def replace_none_with_zero(l):
+#         return [0 if i == None else i for i in l]
+#
+#     x1 = tf.Variable([[z1]])
+#     x2 = tf.Variable([[z2]])
+#     x3 = tf.Variable([[z3]])
+#     with tf.GradientTape() as t2:
+#         with tf.GradientTape() as t1:
+#             f = main_f(x1, x2, x3)
+#         g = t1.gradient(f, [x1, x2, x3])
+#         g = replace_none_with_zero(g)
+#     return t2.jacobian(g, [x1, x2, x3])
+
+
+def hessian_f():
+    return np.array([[12.0, 3.0, 0.0], [3.0, 8.0, -1.0], [0.0, -1.0, 6.0]])
+
 
 def grad_f_fast(x1: float, x2: float, x3: float):
-    return np.array([12*x1+3*x2, 8*x2+3*x1-x3+10, 6*x3-x2])
+    return np.array([12 * x1 + 3 * x2, 8 * x2 + 3 * x1 - x3 + 10, 6 * x3 - x2])
 
 
 def argmin_t(x1, x2, x3, p1, p2, p3):
-
     def one_main_f(t):
-        return main_f(x1+t*p1, x2+t*p2, x3+t*p3)
+        return main_f(x1 + t * p1, x2 + t * p2, x3 + t * p3)
 
     def der_one_main_f(t: float):
         t_ins = tf.Variable([[t]])
@@ -56,8 +73,8 @@ def argmin_t(x1, x2, x3, p1, p2, p3):
 
 
 def argmin_t_fast(x1, x2, x3, p1, p2, p3):
-    return -(12*x1*p1 + 8*x2*p2 + 6*x3*p3 + 3*x1*p2 + 3*x2*p1 - x2*p3 - x3*p2 + 10*p2)\
-        / (12*p1**2 + 8*p2**2 + 6*p3**2 + 6*p1*p2 - 2*p2*p3)
+    return -(12 * x1 * p1 + 8 * x2 * p2 + 6 * x3 * p3 + 3 * x1 * p2 + 3 * x2 * p1 - x2 * p3 - x3 * p2 + 10 * p2) \
+        / (12 * p1 ** 2 + 8 * p2 ** 2 + 6 * p3 ** 2 + 6 * p1 * p2 - 2 * p2 * p3)
 
 
 class Tasks:
@@ -92,9 +109,9 @@ class Tasks:
             k += 1
 
         xk = xk + -tk * gradient
-        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ', "{0:.3f}".format(xk[2])
+        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ',
+              "{0:.3f}".format(xk[2])
               , '] ||grad(xk)|| = ', "{0:.3f}".format(gradient_norm))
-
 
     def grad_spusk_with_opt_step(self):
 
@@ -123,9 +140,9 @@ class Tasks:
             k += 1
 
         xk = xk + -tk * gradient
-        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ', "{0:.3f}".format(xk[2])
+        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ',
+              "{0:.3f}".format(xk[2])
               , '] ||grad(xk)|| = ', "{0:.3f}".format(gradient_norm))
-
 
     def grad_spusk_with_argmin_opt(self):
 
@@ -152,9 +169,83 @@ class Tasks:
             k += 1
 
         xk = xk + -tk * gradient
-        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ', "{0:.3f}".format(xk[2])
+        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ',
+              "{0:.3f}".format(xk[2])
+              , '] ||grad(xk)|| = ', "{0:.3f}".format(gradient_norm))
+
+    def ordinate_spusk(self):
+
+        eps = self.eps
+        xk = self.xk
+        grudfunc = self.gradfunc
+        argminfunc = self.argminfunc
+
+        def choose_grad(xk, k):
+            if k % 3 == 0:
+                answ = np.array([grudfunc(xk[0], xk[1], xk[2])[0], 0.0, 0.0])
+            if k % 3 == 1:
+                answ = np.array([0.0, grudfunc(xk[0], xk[1], xk[2])[1], 0.0])
+            if k % 3 == 2:
+                answ = np.array([0.0, 0.0, grudfunc(xk[0], xk[1], xk[2])[2]])
+
+            if answ[0] == 0.0 and answ[1] == 0.0 and answ[2] ==0.0:
+                answ = grudfunc(xk[0], xk[1], xk[2])
+            return answ
+
+
+        gradient = choose_grad(xk, 0)
+        gradient_norm = linalg.norm(gradient)
+
+        tk = abs(argminfunc(xk[0], xk[1], xk[2], gradient[0], gradient[1], gradient[2]))
+
+        print('x0=', list(xk), end='\n')
+        k = 1
+
+        while gradient_norm > eps:
+            xk = xk + -tk * gradient
+            print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ',
+                  "{0:.3f}".format(xk[2]), '] ||grad(xk)|| = ', "{0:.3f}".format(gradient_norm))
+            gradient = choose_grad(xk, k)
+            gradient_norm = linalg.norm(gradient)
+            tk = abs(argminfunc(xk[0], xk[1], xk[2], gradient[0], gradient[1], gradient[2]))
+            k += 1
+
+        xk = xk + -tk * gradient
+        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ',
+              "{0:.3f}".format(xk[2])
+              , '] ||grad(xk)|| = ', "{0:.3f}".format(gradient_norm))
+
+
+    def Newton(self):
+        eps = self.eps
+        xk = self.xk
+        grudfunc = self.gradfunc
+        argminfunc = self.argminfunc
+
+        gradient = grudfunc(xk[0], xk[1], xk[2])
+        pk = np.dot(linalg.inv(hessian_f()), gradient)
+        gradient_norm = linalg.norm(gradient)
+
+        tk = abs(argminfunc(xk[0], xk[1], xk[2], gradient[0], gradient[1], gradient[2]))
+
+        print('x0=', list(xk), end='\n')
+        k = 1
+
+        while gradient_norm > eps:
+            xk = xk + -tk * pk
+            print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ',
+                  "{0:.3f}".format(xk[2]), '] ||grad(xk)|| = ', "{0:.3f}".format(gradient_norm))
+            gradient = grudfunc(xk[0], xk[1], xk[2])
+            gradient_norm = linalg.norm(gradient)
+            tk = abs(argminfunc(xk[0], xk[1], xk[2], gradient[0], gradient[1], gradient[2]))
+            k += 1
+
+        xk = xk + -tk * pk
+        print('k = ', k, '  ', 'xk = [', "{0:.3f}".format(xk[0]), ' ', "{0:.3f}".format(xk[1]), ' ',
+              "{0:.3f}".format(xk[2])
               , '] ||grad(xk)|| = ', "{0:.3f}".format(gradient_norm))
 
 
 Launch = Tasks(0.01, np.array([0.0, 0.0, 0.0]), True)
-Launch.grad_spusk_with_argmin_opt()
+Launch.Newton()
+
